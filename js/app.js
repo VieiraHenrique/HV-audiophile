de2492290109d48725e6f71671ee4f970109971d
Henrique Vieira <henrique@mvstudio.be>
2022-08-24 15:51:36 +0200

    wip

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".menu-mobile");
+const header = document.querySelector(".header");
 
 hamburger.addEventListener("click", () => {
     mobileNav.classList.toggle("opened");
 });
+
+window.onscroll = () => {
+    if (document.documentElement.scrollTop > 80) {
+        header.classList.add("small-header");
+    } else {
+        header.classList.remove("small-header");
+    }
+};
